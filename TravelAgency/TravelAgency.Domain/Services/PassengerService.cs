922cc2a567e3c0732e9a464862791497f88399f0
Willians Melendez <willianmelendez@gmail.com>
2021-06-01 20:51:30 -0400

    Servicios

---
+++ TravelAgency/TravelAgency.Domain/Services/PassengerService.cs
@@ -0,0 +1,20 @@
+﻿namespace TravelAgency.Domain.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TravelAgency.Domain.Entities;
+    using TravelAgency.Domain.Interfaces;
+
+    class PassengerService
+    {
+        private readonly IAsyncRepository<Passenger> repository;
+
+        public PassengerService(IAsyncRepository<Passenger> repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
